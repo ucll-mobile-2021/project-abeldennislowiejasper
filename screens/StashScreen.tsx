0288ed60c70a792be15ba7427300074475fb3dfd
Abel Van den Briel <abel.vandenbriel@student.ucll.be>
2020-10-15 17:50:54 +0200

    update

--- screens/StashScreen.tsx
+++ screens/StashScreen.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { View, Text, Button, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,35 +11,36 @@ let kaka = new Item(4321, "kaka", "d", "ladieda");
 
 let items = [soep, kaka];
 
-class Stash extends Component{
-  constructor(props){
+class Stash extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-       nr: items.length
+      nr: items.length
     }
   }
 
-  render(){
+  render() {
     setTimeout(() => {
-      this.setState({nr: items.length})
+      this.setState({ nr: items.length })
     }, 1000)
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Stash Screen</Text>
-      <FlatList key={items.length} data={items} extraData={this.state} keyExtractor={item=>item.barcode + ""} renderItem={({item}) => <Text style={[{fontSize: 25}]}>{item.name}</Text>}/>
-      {/* { items.map((item, key)=>(
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Stash Screen</Text>
+        <FlatList key={items.length} data={items} extraData={this.state} keyExtractor={item => item.barcode + ""} renderItem={({ item }) => <Text style={[{ fontSize: 25 }]}>{item.name}</Text>} />
+        {/* { items.map((item, key)=>(
          <Text key={key} style={({fontSize:25})} onPress={()=>navigation.navigate('ItemScreen', item)}> { item.name } </Text>)
          )} */}
-    </View>
-  );}
+      </View>
+    );
+  }
 
 }
 
-export function getItemsLength(){
+export function getItemsLength() {
   return items.length;
 }
 
-export function updateItems(item:Item){
+export function updateItems(item: Item) {
   items.push(item);
 }
 
